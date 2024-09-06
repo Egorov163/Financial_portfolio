252b72640a02d9c1e716dd714f20250dcf69b717
Aleksandr Egorov <151774638+Egorov163@users.noreply.github.com>
2024-09-06 13:24:19 +0400

    Delete all_works/TheBestApp directory

--- all_works/TheBestApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TheBestApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
